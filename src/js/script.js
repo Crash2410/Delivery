78d86eb22a6fcc4b3be4931fe3a0bd0235951533
Artem <artem.xolod.90@gmail.com>
2020-08-23 22:44:42 +0300

    Адаптация 9 блока

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,32 @@ $(document).ready(function () {
     speed: 1200,
     centerMode: true,
     centerPadding: 0,
-    slidesToShow: 3,
     variableWidth: true,
     adaptiveHeight: false,
     prevArrow:
       '<button type="button" class="slick-prev" aria-label="Previous"><img src ="../../icons/arrow_left.png"></button>',
     nextArrow:
       '<button type="button" class="slick-next" aria-label="Next"><img src ="../../icons/arrow_right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
   });
 });
